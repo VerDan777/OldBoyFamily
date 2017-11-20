eb10371c307e749c2f21c04efd6de6c68646c3c6
Verdan777 <vereshhagin98@mail.ru>
2017-11-20 16:52:13 +0200

    Mobile Menu added

---
+++ src/js/modules/MobileMenu.js
@@ -0,0 +1,18 @@
+var $ = require("jquery");
+
+function MobileMenu() {
+var $mainMenu = $(".main-menu");
+var $menuIcon = $(".menu-icon");
+var $menuLinks = $(".main-menu__link");
+
+$menuIcon.on("click", toggleMenu);
+$menuLinks.on("click", toggleMenu);
+$menuLinks.on("tap", toggleMenu);
+
+function toggleMenu() {
+$mainMenu.toggleClass("main-menu--shown");
+$menuIcon.toggleClass("menu-icon--close-x");
+}
+}
+
+module.exports = new MobileMenu;
