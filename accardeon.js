20a992faa7a6b705831a8050c08437ba53a42ebd
Verdan777 <vereshhagin98@mail.ru>
2017-11-23 13:09:10 +0200

    Update 2017-11-23T11:09:05.920Z

--- accardeon.js
+++ accardeon.js
@@ -4,10 +4,13 @@ $('.main-menu__icon').click(function() {
   if ($this.next().hasClass('main-menu__dropdown--shown')) {
       $this.next().removeClass('main-menu__dropdown--shown');
       $this.next().slideUp(350);
+      $this.toggleClass('main-menu__icon--rotate')
+      
   } else {
       $this.parent().parent().find('li .main-menu__dropdown').removeClass('shown');
       $this.parent().find('.main-menu__dropdown').slideUp(250);
       $this.next().slideToggle(250);
       $this.next().toggleClass('main-menu__dropdown--shown');
+      $this.toggleClass('main-menu__icon--rotate')
   }
 });
