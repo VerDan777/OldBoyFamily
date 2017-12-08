1fdf2cb89eb08927f7a0068a8fc77adc714be1a5
Verdan777 <vereshhagin98@mail.ru>
2017-12-08 16:04:42 +0200

    active tab func added

--- src/js/modules/Tabs.js
+++ src/js/modules/Tabs.js
@@ -5,9 +5,9 @@ function Tabs() {
 
 function TabsChanged() {
     var tab_id = $(this).attr('data-tab');
-    $('.tabs__item').removeClass('tabs__current');
+    $('.tabs__item').removeClass('tabs__current tabs__active-bg');
     $('.tabs__content').removeClass('tabs__current');
-    $(this).addClass('tabs__current');
+    $(this).addClass('tabs__current tabs__active-bg');
     $("#"+tab_id).addClass('tabs__current');
 }
 }
